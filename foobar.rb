b8ca25980acbeef305b423ef76a50509a96ed625
jeremymary <jeremyjohnmary@gmail.com>
2017-09-19 20:51:14 -0700

    finished HW1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,20 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    #break if a.length == 0
+
+    a.map! do |x|
+      x = x.to_i
+      x += 2
+    end
+
+    a.delete_if {|x| x%2 != 0}
+    a.uniq!
+    a.delete_if {|x| x>10}
+
+    sum = 0
+    a.each {|x| sum += x}
+    sum
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,21 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  sum = a + b
+  answer = sum ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort!
+  a.map! do |x|
+      x += 1
+    end
+  a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  full_name = first_name + " " + last_name
 end
 
 def blockin_time(a)
